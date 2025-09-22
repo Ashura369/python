2c82e5be1f68b0cc304af184cdce763af2cc8010
Biswajeet Pradhan <biswajeetpradhan36933@gmail.com>
2025-09-22 01:27:26 +0000

    ftjt

--- python practice/0.py
+++ python practice/0.py
@@ -1,22 +1,20 @@
-# %%
 import numpy as np
-import matplotlib.pyplot as plt
 
-# %%
-# 2x2 matrix
-matrix = np.array([[1, 3],
-                   [2, 4]])
 
-# Take each row as (x, y) points
-x = matrix[:, 0]  # first column
-y = matrix[:, 1]  # second column
+a = np.array([1, 2, 3, 4, 5])            # 1D array from list
+b = np.arange(0, 10, 2)                   # 0 to 9 with step 2
+c = np.zeros((2, 3))                      # 2x3 array of zeros
+d = np.ones((3, 2))                       # 3x2 array of ones
+e = np.eye(3)                             # 3x3 identity matrix
+f = np.linspace(0, 1, 5)                  # 5 points evenly spaced between 0 and 1
+g = np.random.rand(2, 2)                  # 2x2 random numbers between 0 and 1
+h = np.random.randint(0, 10, size=(3, 3)) # 3x3 random integers from 0 to 9
 
-# %%
-# Plot the points and connect with a line
-plt.plot(x, y, marker='o', linestyle='-', color='blue', label='2x2 points')
-plt.title("2x2 Matrix Graph")
-plt.xlabel("First column")
-plt.ylabel("Second column")
-plt.grid(True)
-plt.legend()
-plt.show()
+print("a:", a)
+print("b:", b)
+print("c:\n", c)
+print("d:\n", d)
+print("e:\n", e)
+print("f:", f)
+print("g:\n", g)
+print("h:\n", h)
