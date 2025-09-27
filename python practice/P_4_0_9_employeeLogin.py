28f461c1bb6a39b8c360e6df3e0defbebcc958ed
Biswajeet Pradhan <biswajeetpradhan36933@gmail.com>
2025-09-27 22:45:23 +0000

    rhtj

--- python practice/P_4_0_9_employeeLogin.py
+++ python practice/P_4_0_9_employeeLogin.py
@@ -1,5 +1,8 @@
 # taking names as input
 # print welcome message to everyone
+# printing different messages for admins
+# add working hours
+
 emp = [
     "Amit", "Priya", "Rahul", "Sneha", "Vikram",
     "Anjali", "Rohit", "Pooja", "Karan", "Neha",
@@ -7,8 +10,8 @@ emp = [
     "Kavita", "Deepak", "Shreya", "Manoj", "Ritu"
 ]
 
-admin = "shreya"
-present = 3
+admins = ["shreya", "Amit"]
+present = 3     # change the number to increase or decrease the number of emps to be present
 attendence = []
 
 
@@ -16,21 +19,21 @@ for i in range(present):
     name = input(f"ENTER NAME : ")
 
     # greeting with good morning
-    if name.lower() == admin.lower():
-        print("HELLO ADMIN, WELCOME TO THE EXECUTIVE DESK")
+    if name.lower() in [a.lower() for a in admins]:
+        print(f"HELLO!!!, WELCOME TO THE EXECUTIVE DESK {name.title()}\n")
     
     # Normal employee greeting
     elif name.lower() in [e.lower() for e in emp]:          # here we used elif bcoz, admin name is also on the list. And we wanted to avoid normal greeting for the admin. The elif function does this perfectly
-        print("GOOD MORNING! WELCOME TO THE OFFICE")
+        print(f"GOOD MORNING! WELCOME TO THE OFFICE {name.title()}\n")
 
     else:
-        print("NAME NOT FOUND IN EMPLOYEE LIST")
+        print(f"NAME {name} NOT FOUND IN EMPLOYEE LIST\n")
     
     attendence.append(name)
 
 absent = list(set(emp) - set(attendence))
 
-
+print()
 print(f"TOTAL EMPLOYEE PRESENT {len(attendence)} \n\t {attendence}")
 print(f"EMPLOYEE ABSENT ARE {len(absent)} \n\t {absent}")
 
